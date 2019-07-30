d771b7b93dd1005a03965a6791d65265d9edb488
Thiago Santiago <thiago.vps20@gmail.com>
2019-07-30 14:56:58 -0300

    Add rotas de usuarios

--- build/server/api/routes/routes.js
+++ build/server/api/routes/routes.js
@@ -1,13 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var routes_1 = require("../../modules/User/routes");
 var Routes = /** @class */ (function () {
     function Routes(app) {
+        this.userRoutes = new routes_1.default();
         this.getRoutes(app);
     }
     Routes.prototype.getRoutes = function (app) {
-        app.route('/').get(function (req, res) {
-            res.send('Hello, world!');
-        });
+        app.route('/').get(function (req, res) { return res.send('Hello, world!'); });
+        this.getUserRoutes(app);
+    };
+    Routes.prototype.getUserRoutes = function (app) {
+        app.route('/api/users').get(this.userRoutes.index);
+        app.route('/api/users').post(this.userRoutes.create);
+        app.route('/api/users/:id').get(this.userRoutes.findOnde);
+        app.route('/api/users/:id').put(this.userRoutes.update);
+        app.route('/api/users/:id').delete(this.userRoutes.delete);
     };
     return Routes;
 }());
--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,15 +1,26 @@
 import { Application } from 'express';
+import UserRoutes from '../../modules/User/routes';
 
 class Routes {
 
+    private userRoutes: UserRoutes;
+
     constructor(app: Application){
+        this.userRoutes = new UserRoutes();
         this.getRoutes(app);
     }
 
     getRoutes(app: Application): void{
-        app.route('/').get((req, res) => {
-            res.send('Hello, world!');
-        })
+        app.route('/').get((req, res) => res.send('Hello, world!'));
+        this.getUserRoutes(app);
+    }
+
+    getUserRoutes(app: Application): void {
+        app.route('/api/users').get(this.userRoutes.index);
+        app.route('/api/users').post(this.userRoutes.create);
+        app.route('/api/users/:id').get(this.userRoutes.findOnde);
+        app.route('/api/users/:id').put(this.userRoutes.update);
+        app.route('/api/users/:id').delete(this.userRoutes.delete);
     }
 }
 
