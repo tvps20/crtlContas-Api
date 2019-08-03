dc8e5a894a71548877557b71f2802728dfd13eec
Thiago Santiago <thiago.vps20@gmail.com>
2019-08-02 22:20:21 -0300

    Tipando variavel

--- server/models/relations/relations.ts
+++ server/models/relations/relations.ts
@@ -1,6 +1,6 @@
-const Relations = (db) => {
-    db.Cartao.hasMany(db.Fatura, {as: 'faturas', foreignKey: 'cartaoId'})
-    db.Fatura.belongsTo(db.Cartao, { foreignKey: 'cartaoId'})
+const Relations = (db: any) => {
+    db.Cartao.hasMany(db.Fatura, { as: 'faturas', foreignKey: 'cartaoId' })
+    db.Fatura.belongsTo(db.Cartao, { foreignKey: 'cartaoId' })
 }
 
 module.exports = Relations;
