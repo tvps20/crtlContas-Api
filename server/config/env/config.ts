26dfe5428d5ab75745913c77a9fce42d8a2de33c
Thiago Santiago <thiago.vps20@gmail.com>
2019-08-02 21:56:17 -0300

    Correções para build no travis

--- server/config/env/config.ts
+++ server/config/env/config.ts
@@ -2,7 +2,7 @@ let extension: string = 'js';
 if (process.env.NODE_ENV == 'development') {
   extension = 'ts';
 }
-if (process.env.NODE_ENV == 'undefined') {
+if (process.env.NODE_ENV == undefined) {
   process.env.NODE_ENV = 'test';
 }
 module.exports = () => require(`../env/${process.env.NODE_ENV}.env.${extension}`);
