3abfcd2c3b2a605b8c671c3088705d2ce657be48
Thiago Santiago <thiago.vps20@gmail.com>
2019-08-02 22:17:48 -0300

    Correções na varivael de ambiente

--- server/config/env/config.ts
+++ server/config/env/config.ts
@@ -1,9 +1,9 @@
-let variavelAmbiente = process.env.NODE_ENV;
+let variavelAmbiente: string = process.env.NODE_ENV;
 let extension: string = 'js';
-if (variavelAmbiente.toString().trim() == 'development') {
+if (variavelAmbiente.trim() == 'development') {
   extension = 'ts';
 }
-if (variavelAmbiente.toString().trim() == undefined) {
+if (variavelAmbiente.trim() == undefined) {
   process.env.NODE_ENV = 'test';
 }
-module.exports = () => require(`../env/${variavelAmbiente.toString().trim()}.env.${extension}`);
+module.exports = () => require(`../env/${variavelAmbiente.trim()}.env.${extension}`);
