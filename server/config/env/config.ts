fea8067ff103015a41017edd3cc5c6a07786d20a
Thiago Santiago <thiago.vps20@gmail.com>
2019-08-02 21:39:16 -0300

    Removendo libs não usadas

--- server/config/env/config.ts
+++ server/config/env/config.ts
@@ -1,5 +1,5 @@
 let extension: string = 'js';
-if(process.env.NODE_ENV.trim() == 'development') {
+if (process.env.NODE_ENV.trim() == 'development') {
   extension = 'ts';
 }
-module.exports = () => require(`../env/${ process.env.NODE_ENV.trim() }.env.${ extension }`);
+module.exports = () => require(`../env/${process.env.NODE_ENV}.env.${extension}`);
